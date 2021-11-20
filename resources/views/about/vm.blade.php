739ce7ac52f4f4a01a7159701cd08a1a1753bc96
johnreynaceda <johnreynaceda@sksu.edu.ph>
2021-11-20 11:49:16 +0800

    November 20, 2021

--- resources/views/about/vm.blade.php
+++ resources/views/about/vm.blade.php
@@ -9,13 +9,11 @@
 
         <div class="img mx-5 mt-5">
             <p class="leading-loose text-justify">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-
+                King David Academy is a
+                premier educational institution recognized for its comprehensive formation
+                of a pupil as loving and lovable person who is physically healthy,
+                intellectually competent, emotionally balanced, socially gracious, morally
+                upright, and spiritually devout.
 
             </p>
         </div>
@@ -23,23 +21,11 @@
 
         <div class="img mx-5 mt-5">
             <p class="leading-loose text-justify">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                anim id est laborum."
-                Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-
-                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-                aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-                dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
-                sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore
-                magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-                suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-                ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-                nulla pariatur?"
-                1914 translation by H. Rackham
+                King David Academy
+                provides and maintains a healthy, loving, caring, safe, and charitable
+                learning environment that promotes and ensures the proper development and
+                realization of the full potential of each pupil, taking a key role in the
+                transformation of society.
             </p>
         </div>
     </div>
